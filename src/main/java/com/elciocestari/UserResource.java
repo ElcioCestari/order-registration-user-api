202526545f673d188c16ca866bd6dc54ab7489b4
ElcioCestari <elciotaira@gmail.com>
2023-03-08 21:48:31 -0300

    feat(feat/main): init project

---
+++ src/main/java/com/elciocestari/UserResource.java
@@ -0,0 +1,16 @@
+package com.elciocestari;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/users")
+public class UserResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String users() {
+        return "users";
+    }
+}
---
+++ src/test/java/com/elciocestari/UserResourceTest.java
@@ -0,0 +1,21 @@
+package com.elciocestari;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class UserResourceTest {
+
+    @Test
+    void users() {
+        given()
+          .when().get("/users")
+          .then()
+             .statusCode(200)
+             .body(is("users"));
+    }
+
+}
