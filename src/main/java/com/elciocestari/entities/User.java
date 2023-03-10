6f5543fbbecf875dd6a0b78aac230364f68d7760
ElcioCestari <elciotaira@gmail.com>
2023-03-09 23:00:04 -0300

    feat(feat/main): imporve users to get user in mongo atlas

--- src/main/java/com/elciocestari/entities/User.java
+++ src/main/java/com/elciocestari/entities/User.java
@@ -4,15 +4,19 @@ import io.quarkus.mongodb.panache.common.MongoEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @MongoEntity(collection = "Users")
 public class User implements Serializable {
-
+    private ObjectId id;
     private String username;
     private String password;
+
+    private Collection authorities;
 }
